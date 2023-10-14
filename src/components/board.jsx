451e8323c1bc3cfdc1dbe12b2ecf48535721fc27
kritgrover <kritgrover@gmail.com>
2023-10-14 16:32:14 -0400

    Finish Leaderboard

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import Profiles from './profiles';
 import { styles } from '../styles';
 import { SectionWrapper } from '../HOC';
@@ -7,51 +7,46 @@ import { textVariant } from '../utils/motion';
 import { db } from '../firebase';
 import { onValue, ref } from 'firebase/database';
 
-let Leaderboard = [];
-
-const fetchData = async () => {
-  const usersRef = ref(db, 'Users');
-  onValue(usersRef, (snapshot) => {
-    const userData = snapshot.val();
-    if (userData) {
-      Leaderboard = Object.keys(userData).map((userId) => {
-        const user = userData[userId];
-        return {
-          name: user.Name.replace('Name: ', ''),
-          total: user.Total,
-          img: user.img,
-        };
-      });
-    }
-  });
-}
-
-fetchData();
-
-
 const Board = () => {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const usersRef = ref(db, 'Users');
+      onValue(usersRef, (snapshot) => {
+        const userData = snapshot.val();
+        if (userData) {
+          const newLeaderboard = Object.keys(userData).map((userId) => {
+            const user = userData[userId];
+            return {
+              name: user.Name.replace('Name: ', ''),
+              total: user.Total,
+              img: user.img,
+            };
+          });
+          // Sort the new leaderboard data
+          newLeaderboard.sort((a, b) => b.total - a.total);
+          // Set the leaderboard state with the new data
+          setLeaderboard(newLeaderboard);
+        }
+      });
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures this effect runs only once
+
   return (
     <motion.div variants={textVariant()}>
-    
       <section className='flex flex-col items-center'>
         <div className="board">
-          <div className={`${styles.mainSectionTitleText} + 'leaderboard border-b-2 border-black pb-2'`}>Leaderboard</div>
-          <Profiles Leaderboard={sort(Leaderboard)}></Profiles>
+          <div className={`${styles.mainSectionTitleText} + 'leaderboard border-b-2 border-black pb-2'`}>
+            Leaderboard
+          </div>
+          <Profiles Leaderboard={leaderboard}></Profiles>
         </div>
       </section>
     </motion.div>
-  )
-}
-
-function sort(data){
-  return data.sort((a, b) => {
-      if ( a.score === b.score){
-          return b.score - a.score;
-      } else{
-          return b.score - a.score;
-      }
-  })
-
-}
+  );
+};
 
 export default SectionWrapper(Board, "");
