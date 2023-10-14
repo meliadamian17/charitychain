fb92666a8e57df27a51a6bf57eab98272ec01c37
kritgrover <kritgrover@gmail.com>
2023-10-14 01:52:01 -0400

    Added Search Bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,20 @@
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+
 const Search = () => {
 
     return (
-        <>
-
-        </>
-    );
+        <div className="searchContainer">
+          <div className="search">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              fullWidth
+              label="Search Here"
+            />
+          </div>
+        </div>
+      );
 };
 
 export default Search;
