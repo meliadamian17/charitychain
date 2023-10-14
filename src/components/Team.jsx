8d288c31db72b56c0654a7528dc22ee7be6c2836
BrandanBurgess <brandanbb13@gmail.com>
2023-10-14 17:12:21 -0400

    About Us Team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -14,7 +14,7 @@ const Team = () => {
                     Meet Our Team
             </div>
             <div className={styles.mainSectionSubText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Meet Chris, Damian, Krit and Brandan, the team behind CharityChain. They are a group of Computer Science and Machine Learning students looking to capitalize on the sense of community great software can foster. In a world where many may feel disconnected despite being closer than we’ve ever been as a species, they believe that paying forward the goodwill of one’s deeds  will help keep us more connected than ever.
             </div>
             </div>
             <div className="aboutImage">
