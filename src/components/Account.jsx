cef831d89c61e9454ff1b374d2355d89d6aab17d
BrandanBurgess <brandanbb13@gmail.com>
2023-10-14 11:27:28 -0400

    Account Page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -6,12 +6,26 @@ import { navLinks } from "../constants";
 import "../index.css";
 import test from "../assets/BLM1.png"
 import { SectionWrapper } from '../HOC';
+// import Community from './Community'
+import Badges from './Badges'
 
 const Account = () => {
     console.log("Account page");
   return (
     <div className='my-main-bg h-[100vh] pt-[111px]'> 
         
+          <div className="flex items-center justify-start">
+            <h1 className="text-4xl font-bold text-dark-olive">Welcome User</h1>
+          </div>
+          <div className="flex items-center justify-start">
+            <h1 className="text-4xl font-bold text-dark-olive">Amount Donated</h1>
+
+            
+          </div>
+          <div className="flex items-center justify-start">
+            <h1 className="text-4xl font-bold text-dark-olive">Badges</h1>
+          </div>
+          
     </div>
   );
 }
