394494a6088546748c66923c50ddf1b128764bf3
the creator <chenxi.yan@gmail.com>
2023-10-14 09:30:48 -0400

    connecting database

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCtdUGvfDMZiaQFNMaVUDgKLPKWfu_mrng",
@@ -13,4 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getDatabase(app);
 export default app;
