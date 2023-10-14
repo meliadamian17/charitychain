3449acabd8fcc2397aa0c7273e417acd2354bcd9
BrandanBurgess <brandanbb13@gmail.com>
2023-10-14 10:08:00 -0400

    Updated firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCtdUGvfDMZiaQFNMaVUDgKLPKWfu_mrng",
@@ -14,5 +14,25 @@ export const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getDatabase(app);
+const database = getDatabase(app);
+
+export const createUserProfileDocument = (user) => {
+  const userRef = ref(database, `Users/${user.uid}`);
+
+  // Check if the user already exists in the database
+  get(userRef)
+    .then((snapshot) => {
+      if (!snapshot.exists()) {
+        // User does not exist, create a new entry
+        set(userRef, {
+          email: user.email,
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error checking user existence:", error);
+    });
+};
+
+
 export default app;
