19a1d187c671e01689338c6bce7e73e3d3949b90
kritgrover <kritgrover@gmail.com>
2023-10-14 20:23:45 -0400

    Added Team logo

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,5 +1,5 @@
 import { styles } from "../styles";
-import Charity_Chain_logo from "../assets/Charity_Chain_logo.png";
+import Team_Logo from "../assets/Team.jpg";
 import { SectionWrapper } from "../HOC";
 import { motion } from "framer-motion";
 import { textVariant } from "../utils/motion";
@@ -17,8 +17,8 @@ const Team = () => {
             Meet Chris, Damian, Krit and Brandan, the team behind CharityChain. They are a group of Computer Science and Machine Learning students looking to capitalize on the sense of community great software can foster. In a world where many may feel disconnected despite being closer than we’ve ever been as a species, they believe that paying forward the goodwill of one’s deeds  will help keep us more connected than ever.
             </div>
             </div>
-            <div className="aboutImage">
-                <img src={Charity_Chain_logo}/>
+            <div className="aboutImage" id="teamImg">
+                <img src={Team_Logo}/>
             </div>
         </div>
         </motion.div>
